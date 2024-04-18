25054dcfaa42594e51cb8a8cfb761560c3a08a9b
nullptr <nullptr@localhost>
2024-04-18 09:26:16 +0000

    chore: reduce rx/tx buffer size

--- porting/himax/we2/el_wire_we2.cpp
+++ porting/himax/we2/el_wire_we2.cpp
@@ -171,8 +171,8 @@ el_err_code_t WireWE2::init() {
 
     this->_is_present = this->i2c ? true : false;
 
-    this->rx_ring_buffer = new lwRingBuffer(1024 * 4);
-    this->tx_ring_buffer = new lwRingBuffer(1024 * 64);
+    this->rx_ring_buffer = new lwRingBuffer(1024);
+    this->tx_ring_buffer = new lwRingBuffer(1024 * 24);
 
     this->wire_read_enable(sizeof(this->tx_buffer));
 
