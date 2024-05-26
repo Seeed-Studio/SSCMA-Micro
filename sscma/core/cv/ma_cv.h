2edf9a499ae0b7f849eeea77d29acb09ecf532ae
LynnL4 <lht856@foxmail.com>
2024-05-26 08:08:23 +0000

    feat: add basic cv function

---
+++ sscma/core/cv/ma_cv.h
@@ -0,0 +1,21 @@
+#ifndef _MA_CV_H_
+#define _MA_CV_H_
+
+#include "core/ma_common.h"
+
+namespace ma::cv {
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ma_err_t convert(const ma_img_t* src, ma_img_t* dst);
+
+#ifdef __cplusplus
+}
+#endif
+
+}  // namespace ma::cv
+
+
+#endif  // _MA_CV_H_
