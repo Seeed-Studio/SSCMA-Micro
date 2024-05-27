9d427ef8852d10ff6a9d921a60e92e44bef68c02
LynnL4 <lht856@foxmail.com>
2024-05-27 02:11:19 +0000

    test: add model test

--- sscma/sscma.h
+++ sscma/sscma.h
@@ -2,6 +2,6 @@
 #include "core/os/ma_os.h"
 #include "core/engine/ma_engine.h"
 #include "core/model/ma_model.h"
-
+#include "core/cv/ma_cv.h"
 
 #include "porting/ma_porting.h"
--- test/test_model.cpp
+++ test/test_model.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+
 #include <sscma.h>
 
 #include "img_meter.h"
@@ -7,14 +8,14 @@
 
 namespace ma::engine {
 
-
 #define TAG               "test::model"
 #define TENSOR_ARENA_SIZE (1024 * 1024)
+
 
 TEST(Model, YOLO) {
     auto* engine = new Engine();
     EXPECT_EQ(MA_OK, engine->init(TENSOR_ARENA_SIZE));
-    EXPECT_EQ(MA_OK, engine->load_model(g_yolo_meter_model_data, g_yolo_meter_model_data_len));
+    EXPECT_EQ(MA_OK, engine->load_model("yolo_meter.tflite"));
     auto*    model = new ma::model::Yolo(engine);
     ma_img_t img;
     img.data   = (uint8_t*)gImage_meter;
@@ -23,12 +24,13 @@ TEST(Model, YOLO) {
     img.height = 240;
     img.format = MA_PIXEL_FORMAT_RGB888;
     img.rotate = MA_PIXEL_ROTATE_0;
+
     EXPECT_EQ(MA_OK, model->run(static_cast<const void*>(&img)));
     auto perf = model->get_perf();
     MA_LOGI(
         TAG, "pre: %ldms, infer: %ldms, post: %ldms", perf.preprocess, perf.run, perf.postprocess);
     auto _results = model->get_results();
-
+    int  value    = 0;
     for (int i = 0; i < _results.size(); i++) {
         auto result = _results[i];
         MA_LOGI(TAG,
@@ -39,7 +41,10 @@ TEST(Model, YOLO) {
                 result.y,
                 result.w,
                 result.h);
+        value = value * 10 + result.target;
     }
+
+    EXPECT_EQ(179295, value);
 
     delete engine;
 }
