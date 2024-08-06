1e0583696de657b426c830a526e48aa24389b81a
nullptr <nullptr@localhost>
2024-08-06 10:19:06 +0000

    refactor: anchor utils

---
+++ sscma/core/utils/ma_anchors.h
@@ -0,0 +1,20 @@
+#ifndef _MA_UTILS_ANCHORS_H
+#define _MA_UTILS_ANCHORS_H
+
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+#include "ma_types.h"
+
+namespace ma::utils {
+
+std::vector<ma_anchor_stride_t> generateAnchorStrides(size_t input_size, std::vector<size_t> strides);
+
+std::vector<std::vector<ma_pt2f_t>> generateAnchorMatrix(const std::vector<ma_anchor_stride_t>& anchor_strides,
+                                                          float                                  shift_right,
+                                                          float                                  shift_down);
+
+}  // namespace ma::utils
+
+#endif
