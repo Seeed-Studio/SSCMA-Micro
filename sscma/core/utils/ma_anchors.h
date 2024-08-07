41a0326666ed03e7951522fb8c39741759e01a02
nullptr <nullptr@localhost>
2024-08-07 07:58:44 +0000

    chore: cleanup

--- sscma/core/utils/ma_anchors.h
+++ sscma/core/utils/ma_anchors.h
@@ -12,8 +12,8 @@ namespace ma::utils {
 std::vector<ma_anchor_stride_t> generateAnchorStrides(size_t input_size, std::vector<size_t> strides);
 
 std::vector<std::vector<ma_pt2f_t>> generateAnchorMatrix(const std::vector<ma_anchor_stride_t>& anchor_strides,
-                                                          float                                  shift_right,
-                                                          float                                  shift_down);
+                                                         float                                  shift_right,
+                                                         float                                  shift_down);
 
 }  // namespace ma::utils
 
