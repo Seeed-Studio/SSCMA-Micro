5d889e291d798278f54030a84352b1de68b81e66
nullptr <nullptr@localhost>
2024-10-12 19:00:34 +0800

    refactor: esp32 models

--- sscma/porting/espressif/esp32s3/ma_device.cpp
+++ sscma/porting/espressif/esp32s3/ma_device.cpp
@@ -7,45 +7,56 @@
 #include <cstring>
 
 
+#include "ma_camera_esp32.h"
 #include "ma_config_board.h"
 #include "ma_storage_lfs.h"
+#include "ma_transport_console.h"
+
+#include <esp_efuse.h>
+#include <esp_efuse_chip.h>
+#include <esp_efuse_table.h>
+#include <hal/efuse_hal.h>
+#include <esp_partition.h>
+#include <spi_flash_mmap.h>
 
 
 extern "C" {
-    
+
 void ma_invoke_pre_hook(void*) {}
 
-void ma_invoke_post_hook(void*) { }
+void ma_invoke_post_hook(void*) {}
+}
 
+static inline uint32_t _device_id_from_efuse() {
+    char id_full[16]{};
+    esp_err_t err = esp_efuse_read_field_blob(ESP_EFUSE_OPTIONAL_UNIQUE_ID, id_full, 16u << 3);
+
+    if (err != ESP_OK) [[unlikely]]
+        return 0ul;
+
+    // Fowler–Noll–Vo hash function
+    uint32_t hash  = 0x811c9dc5;
+    uint32_t prime = 0x1000193;
+    for (size_t i = 0; i < 16; ++i) {
+        uint8_t value = id_full[i];
+        hash          = hash ^ value;
+        hash *= prime;
+    }
+
+    return hash;
 }
 
 namespace ma {
 
 Device::Device() {
     MA_LOGD(MA_TAG, "Initializing device: %s", MA_BOARD_NAME);
-    {
-        m_name = MA_BOARD_NAME;
-    }
+    { m_name = MA_BOARD_NAME; }
 
     MA_LOGD(MA_TAG, "Initializing device version");
-    {
-        m_version = "XIAO(ESP32S3)";
-    }
+    { m_version = "XIAO(ESP32S3)"; }
 
     MA_LOGD(MA_TAG, "Initializing device ID");
-    {
-        // xip_ownership_acquire();
-        // uint8_t id_full[16];
-        // if (xip_safe_enable()) {
-        //     std::memcpy(id_full, reinterpret_cast<const void*>(BASE_ADDR_FLASH1_R_ALIAS + 0x003DF000), sizeof id_full);
-        // } else {
-        //     MA_LOGE(MA_TAG, "Failed to read device ID, using default");
-        // }
-        // xip_ownership_release();
-        // for (size_t i; i < sizeof id_full; ++i) {
-        //     m_id += std::to_string(id_full[i]);
-        // }
-    }
+    { m_id = std::to_string((size_t)_device_id_from_efuse()); }
 
     MA_LOGD(MA_TAG, "Initializing storage");
     {
@@ -69,54 +80,46 @@ Device::Device() {
 
     MA_LOGD(MA_TAG, "Initializing transports");
     {
-//         static Console console;
-//         console.init(nullptr);
-//         m_transports.push_back(&console);
-
-//         static Serial serial;
-//         serial.init(nullptr);
-//         m_transports.push_back(&serial);
-
-// #ifdef MA_CONFIG_BOARD_I2C_SLAVE
-//         static I2C i2c;
-//         i2c.init(nullptr);
-//         m_transports.push_back(&i2c);
-// #endif
+        static Console console;
+        console.init(nullptr);
+        m_transports.push_back(&console);
     }
 
     MA_LOGD(MA_TAG, "Initializing sensors");
     {
-        // static CameraHimax camera(0);
-        // m_sensors.push_back(&camera);
+        static CameraESP32 camera(0);
+        m_sensors.push_back(&camera);
     }
 
     MA_LOGD(MA_TAG, "Initializing models");
     {
-        // xip_ownership_acquire();
-        // if (xip_safe_enable()) {
-        //     const void* address = reinterpret_cast<void*>(BASE_ADDR_FLASH1_R_ALIAS + 0x00400000);
-        //     const size_t size   = 4096;
-        //     size_t id           = 0;
-        //     for (size_t i = 0; i < size; i += 4096) {
-        //         const void* data = address + i;
-        //         if (ma_ntohl(*(static_cast<const uint32_t*>(data) + 1)) == 0x54464C33) {  // 'TFL3'
-        //             ma_model_t model;
-        //             model.id   = ++id;
-        //             model.type = MA_MODEL_TYPE_UNDEFINED;
-        //             model.name = address;
-        //             model.addr = data;
-        //             m_models.push_back(model);
-        //         }
-        //     }
-        // } else {
-        //     MA_LOGE(MA_TAG, "Failed to find models, using default");
-        // }
-        // xip_ownership_release();
-    }
+        const esp_partition_t* partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_UNDEFINED, "models");
+        if (partition) {
+            const uint8_t** mmap = nullptr;
+            uint32_t* handler    = nullptr;
+            esp_err_t ret        = spi_flash_mmap(partition->address, partition->size, SPI_FLASH_MMAP_DATA, reinterpret_cast<const void**>(mmap), handler);
 
-    MA_LOGD(MA_TAG, "Initializing NPU driver");
-    { 
-        // himax_arm_npu_init(true, true); 
+            if (ret != ESP_OK) {
+                MA_LOGE(MA_TAG, "Failed to map models partition");
+                return;
+            }
+            const void* address = *mmap;
+            const size_t size   = partition->size;
+            size_t id           = 0;
+            for (size_t i = 0; i < size; i += 4096) {
+                const void* data = address + i;
+                if (ma_ntohl(*(static_cast<const uint32_t*>(data) + 1)) == 0x54464C33) {  // 'TFL3'
+                    ma_model_t model;
+                    model.id   = ++id;
+                    model.type = MA_MODEL_TYPE_UNDEFINED;
+                    model.name = address;
+                    model.addr = data;
+                    m_models.push_back(model);
+                }
+            }
+        } else {
+            MA_LOGE(MA_TAG, "Failed to find models, using default");
+        }
     }
 }
 
