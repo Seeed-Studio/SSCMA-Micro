7ce6a9b33775cd0ebfe7b286d4f1b2bbe3d44ab5
nullptr <nullptr@localhost>
2024-11-21 10:51:19 +0000

    feat: add halio engine (experimental)

--- sscma/core/ma_types.h
+++ sscma/core/ma_types.h
@@ -67,6 +67,7 @@ typedef enum {
     MA_TENSOR_TYPE_STR  = 12,
     MA_TENSOR_TYPE_BOOL = 13,
     MA_TENSOR_TYPE_BF16 = 14,
+    MA_TENSOR_TYPE_NMS_BBOX_F32 = 15,
 } ma_tensor_type_t;
 
 typedef struct {
@@ -95,6 +96,7 @@ typedef struct {
     } data;
     bool is_physical;  // For physical tensor
     bool is_variable;  // For constant tensor
+    void* external_handler = nullptr;
 } ma_tensor_t;
 
 typedef enum {
