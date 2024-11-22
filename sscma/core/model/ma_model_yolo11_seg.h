b0b88777da31cbdf0e68274d4945091ce125489e
LynnL4 <lht856@foxmail.com>
2024-11-22 06:15:24 +0000

    feat: add support for YOLO11segmentation model

---
+++ sscma/core/model/ma_model_yolo11_seg.h
@@ -0,0 +1,34 @@
+#ifndef _MA_MODEL_YOLO11_SEG_H_
+#define _MA_MODEL_YOLO11_SEG_H_
+
+#include <cstddef>
+#include <cstdint>
+#include <utility>
+#include <vector>
+
+#include "ma_model_segmenter.h"
+
+namespace ma::model {
+
+class Yolo11Seg : public Segmenter {
+private:
+    ma_tensor_t bboxes_;
+    ma_tensor_t protos_;
+    int32_t num_record_;
+    int32_t num_class_;
+
+protected:
+    ma_err_t postprocess() override;
+    
+    ma_err_t postProcessF32();
+
+public:
+    Yolo11Seg(Engine* engine);
+    ~Yolo11Seg();
+
+    static bool isValid(Engine* engine);
+};
+
+}  // namespace ma::model
+
+#endif  // _MA_MODEL_YOLO_H
